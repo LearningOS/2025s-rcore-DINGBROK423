728129bf35288d4ff880323e5ff331901e1836d6
3023244344-DING <1301557108@qq.com>
2025-05-31 10:40:33 -0700

    lab4_up6

---
+++ temp-os/src/syscall/mod.rs
@@ -0,0 +1,112 @@
+//    //! Implementation of syscalls
+// //!
+// //! The single entry point to all system calls, [`syscall()`], is called
+// //! whenever userspace wishes to perform a system call using the `ecall`
+// //! instruction. In this case, the processor raises an 'Environment call from
+// //! U-mode' exception, which is handled as one of the cases in
+// //! [`crate::trap::trap_handler`].
+// //!
+// //! For clarity, each single syscall is implemented as its own function, named
+// //! `sys_` then the name of the syscall. You can find functions like this in
+// //! submodules, and you should also implement syscalls this way.
+// const SYSCALL_WRITE: usize = 64;
+// /// exit syscall
+// const SYSCALL_EXIT: usize = 93;
+// /// yield syscall
+// const SYSCALL_YIELD: usize = 124;
+// /// gettime syscall
+// const SYSCALL_GET_TIME: usize = 169;
+// /// sbrk syscall
+// const SYSCALL_SBRK: usize = 214;
+// /// munmap syscall
+// const SYSCALL_MUNMAP: usize = 215;
+// /// mmap syscall
+// const SYSCALL_MMAP: usize = 222;
+// /// trace syscall
+// const SYSCALL_TRACE: usize = 410;
+// /// gettimeofday syscall
+// pub const SYSCALL_GETTIMEOFDAY: usize = 169;
+// mod fs;
+// mod process;
+
+// use fs::*;
+// use process::*;
+// pub use process::{init_syscall_counter, increment_syscall, count_syscall};
+// /// handle syscall exception with `syscall_id` and other arguments
+// pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+//     increment_syscall(syscall_id);
+
+//     match syscall_id {
+//         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
+//         SYSCALL_EXIT => sys_exit(args[0] as i32),
+//         SYSCALL_YIELD => sys_yield(),
+//         SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
+//         SYSCALL_TRACE => sys_trace(args[0], args[1], args[2]),
+//         SYSCALL_MMAP => sys_mmap(args[0], args[1], args[2]),
+//         SYSCALL_MUNMAP => sys_munmap(args[0], args[1]),
+//         SYSCALL_SBRK => sys_sbrk(args[0] as i32),
+//         _ => panic!("Unsupported syscall_id: {}", syscall_id),
+//     }
+// }
+// /// Initialize syscall tracking
+// pub fn init_syscall_tracking() {
+//     init_syscall_counter();
+// }
+//! Implementation of syscalls
+//!
+//! The single entry point to all system calls, [`syscall()`], is called
+//! whenever userspace wishes to perform a system call using the `ecall`
+//! instruction. In this case, the processor raises an 'Environment call from
+//! U-mode' exception, which is handled as one of the cases in
+//! [`crate::trap::trap_handler`].
+//!
+//! For clarity, each single syscall is implemented as its own function, named
+//! `sys_` then the name of the syscall. You can find functions like this in
+//! submodules, and you should also implement syscalls this way.
+const SYSCALL_WRITE: usize = 64;
+/// exit syscall
+const SYSCALL_EXIT: usize = 93;
+/// yield syscall
+const SYSCALL_YIELD: usize = 124;
+/// gettime syscall
+const SYSCALL_GET_TIME: usize = 169;
+/// sbrk syscall
+const SYSCALL_SBRK: usize = 214;
+/// munmap syscall
+const SYSCALL_MUNMAP: usize = 215;
+/// mmap syscall
+const SYSCALL_MMAP: usize = 222;
+/// trace syscall
+const SYSCALL_TRACE: usize = 410;
+
+/// 一个粗略的上界
+pub const SYSCALL_ID_UPPER_BOUND: usize = 500;
+
+mod fs;
+mod process;
+
+use fs::*;
+use process::*;
+
+/// handle syscall exception with `syscall_id` and other arguments
+pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+    match syscall_id {
+        SYSCALL_WRITE | SYSCALL_EXIT | 
+        SYSCALL_YIELD | SYSCALL_GET_TIME | 
+        SYSCALL_TRACE | SYSCALL_MMAP | SYSCALL_MUNMAP |
+        SYSCALL_SBRK => update_syscall_cnt(syscall_id),
+        _ => panic!("Unsupported syscall_id: {}", syscall_id),
+    };
+
+    match syscall_id {
+        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
+        SYSCALL_EXIT => sys_exit(args[0] as i32),
+        SYSCALL_YIELD => sys_yield(),
+        SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
+        SYSCALL_TRACE => sys_trace(args[0], args[1], args[2]),
+        SYSCALL_MMAP => sys_mmap(args[0], args[1], args[2]),
+        SYSCALL_MUNMAP => sys_munmap(args[0], args[1]),
+        SYSCALL_SBRK => sys_sbrk(args[0] as i32),
+        _ => panic!("Unsupported syscall_id: {}", syscall_id),
+    }
+}
