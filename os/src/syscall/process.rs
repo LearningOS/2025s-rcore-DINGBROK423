cb2f474698c4d085f56107eeb0064ef8207d037e
3023244344-DING <1301557108@qq.com>
2025-06-09 20:48:56 -0700

    tmp

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -110,6 +110,7 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
+    let us=sys
     -1
 }
 
